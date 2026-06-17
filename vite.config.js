316fdbf31f9a9436e8ebb0acac63ada377ec74b4
Aviya <gapa.bhd1@gmail.com>
2026-06-17 17:27:52 +0300

    7 try

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 
-
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc' // או רק '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
   base: '/omri/', // <-- השורה הזו היא הסוד!
 })
